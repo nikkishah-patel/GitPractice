2915507afa5bfe05020bccfb6ab4a8c047651fff
nikkishah-patel <nikkishah.ap@gmail.com>
2018-01-23 14:30:13 -0500

    added new line

---
+++ src/NewGitDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Please enter your first name.");
+		
+		String userName = scnr.nextLine();
+		System.out.println("Your first name is: " + userName);
+		
+		
+
+	}
+
+}
